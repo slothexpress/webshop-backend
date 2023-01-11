2dfeb63a3cc6733670f2d69687caef7625b9579b
slothexpress <hellosami@protonmail.com>
2023-01-11 09:45:42 +0100

    Add Spring Boot project

---
+++ src/main/java/kodtest/omegapoint/OmegapointApplication.java
@@ -0,0 +1,13 @@
+package kodtest.omegapoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmegapointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmegapointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kodtest/omegapoint/OmegapointApplicationTests.java
@@ -0,0 +1,13 @@
+package kodtest.omegapoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmegapointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
