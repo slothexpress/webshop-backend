023473bae7de7e01f16b2d25f0f8518c5a54d549
slothexpress <hellosami@protonmail.com>
2023-01-11 13:25:24 +0100

    Update root endpoint to return all products

--- src/main/java/kodtest/omegapoint/RestController.java
+++ src/main/java/kodtest/omegapoint/RestController.java
@@ -1,13 +1,25 @@
 package kodtest.omegapoint;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
+    Service service;
+
+    public RestController() throws JsonProcessingException {
+        service = new Service();
+    }
+
+    // Root endpoint returns all products
     @GetMapping("/")
-    public String demo() {
-        return "HELLO";
+    public HashMap<Integer, Product> getAllProducts() {
+        HashMap<Integer, Product> allProducts = service.getMockDatabase();
+        return allProducts;
     }
 
 }
