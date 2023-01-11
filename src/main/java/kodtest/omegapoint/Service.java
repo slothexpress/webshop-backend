69bb6498536d4522e097661d78c142546d0174e3
slothexpress <hellosami@protonmail.com>
2023-01-11 13:21:16 +0100

    Add method to parse json data
    Parse json data and map to Product class
    Add products to a mockdatabase

--- src/main/java/kodtest/omegapoint/Service.java
+++ src/main/java/kodtest/omegapoint/Service.java
@@ -1,17 +1,30 @@
 package kodtest.omegapoint;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
 
 @org.springframework.stereotype.Service
 public class Service {
 
-    public Service() {
+    private HashMap<Integer, Product> mockDatabase = new HashMap<>();
+
+    public Service() throws JsonProcessingException {
+        fetchAPI();
     }
 
-    public String fetchAPI() {
+    public HashMap<Integer, Product> getMockDatabase() {
+        return mockDatabase;
+    }
+
+    public String fetchAPI() throws JsonProcessingException {
         String result = "null";
         String line;
         StringBuffer content = new StringBuffer();
@@ -37,6 +50,36 @@ public class Service {
             System.out.println("Fetch threw exception: " + e);
         }
         result = content.toString();
+        saveProducts(result);
         return result;
+    }
+
+    public void saveProducts(String json) throws JsonProcessingException {
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode = mapper.readTree(json);
+
+
+        List<JsonNode> jsonObjects = rootNode.findParents("rating");
+        HashMap<Integer, Product> products = new HashMap();
+
+        for (JsonNode object : jsonObjects) {
+            int id = Integer.parseInt(object.get("id").toString());
+            String title = object.get("title").toString();
+            double price = Double.parseDouble(object.get("price").toString());
+            String description = object.get("description").toString();
+            String category = object.get("category").toString();
+            String image = object.get("image").toString();
+
+            JsonNode ratingObject = object.get("rating");
+            double rate = Double.parseDouble(ratingObject.get("rate").toString());
+            int count = Integer.parseInt(ratingObject.get("count").toString());
+            Rating rating = new Rating(rate, count);
+
+            Product product = new Product(title, price, description, category, image, rating);
+            products.put(id, product);
+        }
+
+        mockDatabase = products;
     }
 }
