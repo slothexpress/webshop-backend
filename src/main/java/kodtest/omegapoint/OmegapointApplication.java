9f086e80d7f3aabf2b5b085b9a2ec29bec351b95
slothexpress <hellosami@protonmail.com>
2023-01-11 12:19:33 +0100

    Put HTTP connection in try catch block, and disconnect

--- src/main/java/kodtest/omegapoint/OmegapointApplication.java
+++ src/main/java/kodtest/omegapoint/OmegapointApplication.java
@@ -1,5 +1,9 @@
 package kodtest.omegapoint;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +14,10 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 public class OmegapointApplication {
@@ -18,25 +26,30 @@ public class OmegapointApplication {
 
 		SpringApplication.run(OmegapointApplication.class, args);
 
-		URL url = new URL("https://kodtest.azurewebsites.net/api/products?code=MWZOJunmBNEPDGxldyIKSplsqq/8Sv4c6KvgZ1vyh4Z9wCaw6rqJIA==");
-		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-		connection.setRequestMethod("GET");
-		connection.connect();
+		String line;
+		StringBuffer content = new StringBuffer();
 
-		int responseCode = connection.getResponseCode();
+		try {
+			URL url = new URL("https://kodtest.azurewebsites.net/api/products?code=MWZOJunmBNEPDGxldyIKSplsqq/8Sv4c6KvgZ1vyh4Z9wCaw6rqJIA==");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.connect();
 
-		if(responseCode == 200) {
-			BufferedReader data = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			int responseCode = connection.getResponseCode();
 
-			String line;
-			StringBuffer content = new StringBuffer();
+			if(responseCode == 200) {
+				BufferedReader data = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
-			while((line = data.readLine()) != null) {
-				content.append(line);
+				while((line = data.readLine()) != null) {
+					content.append(line);
+				}
 			}
-			System.out.println(content);
-		}
+			connection.disconnect();
 
+		} catch (Exception e) {
+			System.out.println("Fetch threw exception: " + e);
+		}
+		
 	}
 
 }
