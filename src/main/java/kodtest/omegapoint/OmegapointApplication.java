8449fe67520271f8554e6b7b611ffe8ff7cd3306
slothexpress <hellosami@protonmail.com>
2023-01-11 10:06:14 +0100

    Test: Http connection and responsecode 200

--- src/main/java/kodtest/omegapoint/OmegapointApplication.java
+++ src/main/java/kodtest/omegapoint/OmegapointApplication.java
@@ -3,11 +3,28 @@ package kodtest.omegapoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
 @SpringBootApplication
 public class OmegapointApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+
 		SpringApplication.run(OmegapointApplication.class, args);
+
+		URL url = new URL("https://kodtest.azurewebsites.net/api/products?code=MWZOJunmBNEPDGxldyIKSplsqq/8Sv4c6KvgZ1vyh4Z9wCaw6rqJIA==");
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod("GET");
+		connection.connect();
+
+		int responseCode = connection.getResponseCode();
+
+		System.out.println("OK..." + responseCode);
+
 	}
 
 }
